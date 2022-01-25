865479ecade608068c5dbadd417957dc2e662b90
Edsonfrs <edsonfrs@gmail.com>
2022-01-25 17:19:02 -0300

    Criação e configuração dos arquivos de rotas

---
+++ crud-angular/src/app/books/books-routing.module.ts
@@ -0,0 +1,13 @@
+import { BooksComponent } from './books/books.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: BooksComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BooksRoutingModule { }
