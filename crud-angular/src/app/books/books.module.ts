95a37d64b7bc85c99944af376420bdf6eba8313a
Edsonfrs <edsonfrs@gmail.com>
2022-01-27 21:55:00 -0300

    Tratamento de erros na página de listagem de itens

--- crud-angular/src/app/books/books.module.ts
+++ crud-angular/src/app/books/books.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books/books.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
 
 
 
@@ -16,7 +17,8 @@ import { AppMaterialModule } from '../shared/app-material/app-material.module';
   imports: [
     CommonModule,
     BooksRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    SharedModule
   ]
 })
 export class BooksModule { }
--- crud-angular/src/app/books/books/books.component.ts
+++ crud-angular/src/app/books/books/books.component.ts
@@ -1,7 +1,9 @@
 import { BooksService } from './../services/books.service';
 import { Book } from './../model/book';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-books',
@@ -16,10 +18,26 @@ export class BooksComponent implements OnInit {
   //booksService: BooksService;
 
 
-  constructor(private booksService: BooksService ) {
+  constructor(
+    private booksService: BooksService,
+    public dialog: MatDialog
+     ) {
     //this.books = [];
     //this.booksService = new BooksService();
-    this.books$ = this.booksService.list();
+    this.books$ = this.booksService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar lista de livros!');
+        return of([])
+      })
+      );
+
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
